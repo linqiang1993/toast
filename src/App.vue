37be0f2c51e484a482ab5c042b7571305f276991
广东易臣 <yc@192.168.0.131>
2021-01-26 16:10:49 +0800

    提交

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+   <toast :msg = "'测试'" :isShow = "true"/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      
+    }
+  }
+}
+</script>
+
+<style scope lang="scss">
---
+++ src/main.js
@@ -0,0 +1,8 @@
+import Vue from 'vue'
+import App from './App.vue'
+import Toast from './lib/toast.js'
+Vue.use(Toast)
+new Vue({
+  el: '#app',
+  render: h => h(App)
+})
